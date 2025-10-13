1981f6a388bee3a2999efd60ce503bcbae2e6316
GyulyVGC <gyulyvgc99@gmail.com>
2025-10-13 16:47:49 +0200

    spawn a separate thread for each interface and traffic direction

--- nullnet-user/src/ebpf/ingress.rs
+++ nullnet-user/src/ebpf/ingress.rs
@@ -24,187 +24,89 @@ use super::{
 pub fn load_ingress(
     terminate: Arc<AtomicBool>,
 ) {
-    thread::spawn({
-        move || {
-            let Ok(ifaces) = pcap::Device::list() else { return; };
-            let ifaces_names: Vec<String> = ifaces.iter().map(|d| d.name.to_owned()).collect();
+    let Ok(ifaces) = pcap::Device::list() else { return; };
+    let ifaces_names: Vec<String> = ifaces.iter().map(|d| d.name.to_owned()).collect();
 
-            let rlim = libc::rlimit {
-                rlim_cur: libc::RLIM_INFINITY,
-                rlim_max: libc::RLIM_INFINITY,
-            };
+    for direction in [TcAttachType::Ingress, TcAttachType::Egress] {
+        for iface_name in &ifaces_names {
+            thread::spawn({
+                move || {
+                    let rlim = libc::rlimit {
+                        rlim_cur: libc::RLIM_INFINITY,
+                        rlim_max: libc::RLIM_INFINITY,
+                    };
 
-            unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlim) };
+                    unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlim) };
 
-            let traffic_direction = EbpfTrafficDirection::Ingress as i32;
+                    let mut bpf = match EbpfLoader::new()
+                        .load(include_bytes_aligned!(env!("NULLNET_BIN_PATH")))
+                    {
+                        Ok(v) => v,
+                        Err(e) => {
+                            error!("Failed to load the ingress eBPF bytecode. {}", e);
+                            continue;
+                        }
+                    };
 
-            let mut bpf = match EbpfLoader::new()
-                .set_global("TRAFFIC_DIRECTION", &traffic_direction, true)
-                .load(include_bytes_aligned!(env!("NULLNET_BIN_PATH")))
-            {
-                Ok(v) => v,
-                Err(e) => {
-                    error!("Failed to load the ingress eBPF bytecode. {}", e);
-                    // Notification::send(
-                    //     "Failed to load the ingress eBPF bytecode",
-                    //     NotificationLevel::Error,
-                    //     notification_sender,
-                    // )
-                    // .unwrap();
-                    return;
-                }
-            };
+                    let _ = tc::qdisc_add_clsact(iface_name);
 
-            for iface_name in &ifaces_names {
-                let _ = tc::qdisc_add_clsact(iface_name);
-            }
+                    let program: &mut SchedClassifier =
+                        bpf.program_mut("nullnet").unwrap().try_into().unwrap();
 
-            let program: &mut SchedClassifier =
-                bpf.program_mut("nullnet").unwrap().try_into().unwrap();
+                    if let Err(e) = program.load() {
+                        error!("Failed to load the ingress eBPF program to the kernel. {e}",);
+                        continue;
+                    };
 
-            if let Err(e) = program.load() {
-                error!("Failed to load the ingress eBPF program to the kernel. {e}",);
-                // Notification::send(
-                //     "Failed to load the ingress eBPF program to the kernel",
-                //     NotificationLevel::Error,
-                //     notification_sender,
-                // )
-                // .unwrap();
-                return;
-            };
-
-            for direction in [TcAttachType::Ingress, TcAttachType::Egress] {
-                for iface_name in &ifaces_names {
                     if let Err(e) = program.attach(iface_name, direction) {
                         error!("Failed to attach the ingress eBPF program to the interface. {e}",);
-                        // Notification::send(
-                        //     "Failed to attach the ingress eBPF program to the interface",
-                        //     NotificationLevel::Error,
-                        //     notification_sender,
-                        // )
-                        // .unwrap();
-                        return;
+                        continue;
                     };
-                }
-            }
 
-            let mut poll = Poll::new().unwrap();
-            let mut events = Events::with_capacity(128);
+                    let mut poll = Poll::new().unwrap();
+                    let mut events = Events::with_capacity(128);
 
-            //filter-ebpf interface
-            let mut transport_filters: Array<_, u32> =
-                Array::try_from(bpf.take_map("TRANSPORT_FILTERS").unwrap()).unwrap();
+                    // packets reader
+                    let mut ring_buf = RingBuffer::new(&mut bpf);
 
-            let mut network_filters: Array<_, u32> =
-                Array::try_from(bpf.take_map("NETWORK_FILTERS").unwrap()).unwrap();
+                    poll.registry()
+                        .register(
+                            &mut SourceFd(&ring_buf.buffer.as_raw_fd()),
+                            Token(0),
+                            Interest::READABLE,
+                        )
+                        .unwrap();
 
-            let mut link_filters: Array<_, u32> =
-                Array::try_from(bpf.take_map("LINK_FILTERS").unwrap()).unwrap();
-
-            let mut traffic_direction_filter: Array<_, u8> =
-                Array::try_from(bpf.take_map("TRAFFIC_DIRECTION_FILTER").unwrap()).unwrap();
-
-            // firewall-ebpf interface
-            let mut ipv4_firewall: HashMap<_, u32, [u16; MAX_RULES_PORT]> =
-                HashMap::try_from(bpf.take_map("BLOCKLIST_IPV4").unwrap()).unwrap();
-
-            let mut ipv6_firewall: HashMap<_, u128, [u16; MAX_RULES_PORT]> =
-                HashMap::try_from(bpf.take_map("BLOCKLIST_IPV6").unwrap()).unwrap();
-
-            // firewall thread
-            // thread::spawn(move || {
-            //     loop {
-            //         if let Ok(signal) = firewall_ingress_receiver.recv() {
-            //             match signal {
-            //                 FirewallSignal::Rule(rule) => match rule.ip {
-            //                     IpAddr::V4(addr) => update_ipv4_blocklist(
-            //                         &mut ipv4_firewall,
-            //                         addr,
-            //                         rule.port,
-            //                         rule.enabled,
-            //                     ),
-            //
-            //                     IpAddr::V6(addr) => update_ipv6_blocklist(
-            //                         &mut ipv6_firewall,
-            //                         addr,
-            //                         rule.port,
-            //                         rule.enabled,
-            //                     ),
-            //                 },
-            //                 FirewallSignal::Kill => {
-            //                     break;
-            //                 }
-            //             }
-            //         }
-            //     }
-            // });
-
-            // packets filters thread
-            // thread::spawn(move || {
-            //     loop {
-            //         if let Ok(signal) = filter_channel_receiver.recv() {
-            //             match signal {
-            //                 FilterChannelSignal::ProtoUpdate((filter, flag)) => match filter {
-            //                     Protocol::Transport(p) => {
-            //                         let _ = transport_filters.set(p as u32, flag as u32, 0);
-            //                     }
-            //                     Protocol::Network(p) => {
-            //                         let _ = network_filters.set(p as u32, flag as u32, 0);
-            //                     }
-            //                     Protocol::Link(p) => {
-            //                         let _ = link_filters.set(p as u32, flag as u32, 0);
-            //                     }
-            //                 },
-            //                 FilterChannelSignal::DirectionUpdate(flag) => {
-            //                     let _ = traffic_direction_filter.set(0, flag as u8, 0);
-            //                 }
-            //                 FilterChannelSignal::Kill => {
-            //                     break;
-            //                 }
-            //             }
-            //         }
-            //     }
-            // });
-
-            // packets reader
-            let mut ring_buf = RingBuffer::new(&mut bpf);
-
-            poll.registry()
-                .register(
-                    &mut SourceFd(&ring_buf.buffer.as_raw_fd()),
-                    Token(0),
-                    Interest::READABLE,
-                )
-                .unwrap();
-
-            loop {
-                poll.poll(&mut events, Some(Duration::from_millis(100)))
-                    .unwrap();
-                if terminate.load(std::sync::atomic::Ordering::Relaxed) {
-                    break;
-                }
-                for event in &events {
-                    if terminate.load(std::sync::atomic::Ordering::Relaxed) {
-                        break;
-                    }
-                    if event.token() == Token(0) && event.is_readable() {
+                    loop {
+                        poll.poll(&mut events, Some(Duration::from_millis(100)))
+                            .unwrap();
                         if terminate.load(std::sync::atomic::Ordering::Relaxed) {
                             break;
                         }
-                        while let Some(item) = ring_buf.next() {
+                        for event in &events {
                             if terminate.load(std::sync::atomic::Ordering::Relaxed) {
                                 break;
                             }
-                            // let data: [u8; RawData::LEN] = item.to_owned().try_into().unwrap();
-                            // data_sender.send((data, TrafficDirection::Ingress)).ok();
+                            if event.token() == Token(0) && event.is_readable() {
+                                if terminate.load(std::sync::atomic::Ordering::Relaxed) {
+                                    break;
+                                }
+                                while let Some(item) = ring_buf.next() {
+                                    if terminate.load(std::sync::atomic::Ordering::Relaxed) {
+                                        break;
+                                    }
+                                    // let data: [u8; RawData::LEN] = item.to_owned().try_into().unwrap();
+                                    // data_sender.send((data, TrafficDirection::Ingress)).ok();
+                                }
+                            }
                         }
                     }
-                }
-            }
 
-            let _ = poll
-                .registry()
-                .deregister(&mut SourceFd(&ring_buf.buffer.as_raw_fd()));
+                    let _ = poll
+                        .registry()
+                        .deregister(&mut SourceFd(&ring_buf.buffer.as_raw_fd()));
+                }
+            });
         }
-    });
+    }
 }
