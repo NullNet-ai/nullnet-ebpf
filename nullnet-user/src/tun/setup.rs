cd9c3aa8909a9e1cbfd18a1b79e9816bc51b321d
GyulyVGC <gyulyvgc99@gmail.com>
2025-10-21 17:31:38 +0200

    correctly create tun devices

--- nullnet-user/src/tun/setup.rs
+++ nullnet-user/src/tun/setup.rs
@@ -2,8 +2,12 @@ use tun2::{Configuration};
 use std::net::{IpAddr, Ipv4Addr};
 
 pub(crate) fn setup_tun(name: &str, ip: IpAddr) {
-    // configure TUN device
     let mut config = Configuration::default();
-    config.name(name.to_string());
-    config.mtu(42500).address(ip).netmask(IpAddr::V4(Ipv4Addr::new(255, 255, 255, 0))).up();
+    config
+        .mtu(42500)
+        .tun_name(name)
+        .address(ip)
+        .netmask(IpAddr::V4(Ipv4Addr::new(255, 255, 255, 0)))
+        .up();
+    let _ = tun::create(&config)?;
 }
