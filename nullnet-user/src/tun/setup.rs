fedb9a5a4cea89bea3bd21815760a3341ad661f3
GyulyVGC <gyulyvgc99@gmail.com>
2025-10-21 17:21:21 +0200

    add tun2 dependency

--- nullnet-user/src/tun/setup.rs
+++ nullnet-user/src/tun/setup.rs
@@ -5,5 +5,5 @@ pub(crate) fn setup_tun(name: &str, ip: IpAddr) {
     // configure TUN device
     let mut config = Configuration::default();
     config.name = name.to_string();
-    config.mtu(42500).address(tun_ip).netmask(IpAddr::V4(Ipv4Addr::new(255, 255, 255, 0))).up();
+    config.mtu(42500).address(ip).netmask(IpAddr::V4(Ipv4Addr::new(255, 255, 255, 0))).up();
 }
