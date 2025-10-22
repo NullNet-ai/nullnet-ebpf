c16c5902b2a0594523c6b47e0098cff337ef9d21
GyulyVGC <gyulyvgc99@gmail.com>
2025-10-22 16:24:32 +0200

    add nullnet.service and linux-setup.sh

---
+++ linux-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Update nullnet.service file
+sudo cp nullnet.service /etc/systemd/system/ && \
+sudo systemctl enable nullnet && \
+git checkout main && \
+git pull && \
+cargo xtask build --release && \
+sudo reboot
