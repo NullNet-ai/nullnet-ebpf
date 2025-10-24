e2e8ba180d9f6cf0ef422741401d7d69edad90b4
GyulyVGC <gyulyvgc99@gmail.com>
2025-10-24 16:29:15 +0200

    loop to read from the created tun

--- nullnet-user/src/tun/setup.rs
+++ nullnet-user/src/tun/setup.rs
@@ -14,7 +14,7 @@ pub(crate) fn setup_tun(name: &str, ip: IpAddr) {
 
         let mut buf = [0; 4096];
         loop {
-            let _ = dev.read(&mut buf)?;
+            let _ = tun.read(&mut buf)?;
         }
     });
 }
